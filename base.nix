4fba1d8bde2c6d49f7b057718af3f0a5ba8336fc
Connor Feeley <git@cfeeley.org>
2023-06-10 15:58:16 -0400

    overlay: disable various workarounds

--- base.nix
+++ base.nix
@@ -6,31 +6,31 @@
   # RISC-V Quirks and patches, should be upstreamed
   nixpkgs.overlays = [
     (final: prev: {
-      gusb = prev.gusb.overrideAttrs (old: {
-        buildInputs = old.buildInputs ++ [ prev.gobject-introspection ];
-      });
-      colord = prev.colord.overrideAttrs (old: {
-        buildInputs = old.buildInputs ++ [ prev.gobject-introspection ];
-      });
-      cage = prev.cage.overrideAttrs (old: {
-        depsBuildBuild = [ prev.pkg-config ];
-      });
+      # gusb = prev.gusb.overrideAttrs (old: {
+      #   buildInputs = old.buildInputs ++ [ prev.gobject-introspection ];
+      # });
+      # colord = prev.colord.overrideAttrs (old: {
+      #   buildInputs = old.buildInputs ++ [ prev.gobject-introspection ];
+      # });
+      # cage = prev.cage.overrideAttrs (old: {
+      #   depsBuildBuild = [ prev.pkg-config ];
+      # });
 #      ebook_tools = prev.ebook_tools.overrideAttrs (_old: {
 #        preConfigure = ''
 #          NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE $($PKG_CONFIG --cflags libzip)"
 #        '';
 #      });
-      discount = prev.discount.overrideAttrs (_old: {
-        configurePlatforms = [];
-      });
-      sane-backends = prev.sane-backends.overrideAttrs (_old: {
-        CFLAGS = "-DHAVE_MMAP=0";
-      });
-      curl = prev.curl.overrideAttrs (_old: {
-        CFLAGS = "-w";
-        LDFLAGS = "-latomic";
-      });
-      x265 = prev.x265.override { multibitdepthSupport = false; };
+      # discount = prev.discount.overrideAttrs (_old: {
+      #   configurePlatforms = [];
+      # });
+      # sane-backends = prev.sane-backends.overrideAttrs (_old: {
+      #   CFLAGS = "-DHAVE_MMAP=0";
+      # });
+      # curl = prev.curl.overrideAttrs (_old: {
+      #   CFLAGS = "-w";
+      #   LDFLAGS = "-latomic";
+      # });
+      # x265 = prev.x265.override { multibitdepthSupport = false; };
     })
   ];
 
